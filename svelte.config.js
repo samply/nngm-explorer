93fbec227c38ef483f38e47ecbf317243e3a83ce
David Juarez <d.juarez@dkfz-heidelberg.de>
2025-09-04 15:22:19 +0200

    fix: add adapter to svelte config (II)

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@
 //
 // export default config;
 
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-node';
 import sveltePreprocess from 'svelte-preprocess';
 
 export default {
@@ -25,7 +25,7 @@ export default {
 	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
 
-    kit: {
-        adapter: adapter(),
-    },
+	kit: {
+		adapter: adapter()
+	}
 };

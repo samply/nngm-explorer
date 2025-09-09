8a2714b3c41bb29ce27081b7a7252155845b3543
David Juarez <d.juarez@dkfz-heidelberg.de>
2025-09-09 13:54:10 +0200

    chore: svelte preprocess instead of vite preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -18,12 +18,12 @@
 // export default config;
 
 import adapter from '@sveltejs/adapter-node';
-import sveltePreprocess from 'svelte-preprocess';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()

24c09a9a5862000d768a4446decd5e9bf2064a20
David Juarez <d.juarez@dkfz-heidelberg.de>
2025-09-16 17:20:51 +0200

    feat: add productive options

--- src/lib/env-options.ts
+++ src/lib/env-options.ts
@@ -1,10 +1,10 @@
 import { env } from '$env/dynamic/public';
 import type { Options } from './options';
-//import optionsProd from '../config/options.json';
+import optionsProd from '../config/options.json';
 import optionsTest from '../config/options-test.json';
 
 /** The options depending on the environment */
-export let options: Options = optionsTest;
+export let options: Options = optionsProd;
 if (env.PUBLIC_ENVIRONMENT === 'test') {
 	options = optionsTest;
 }
